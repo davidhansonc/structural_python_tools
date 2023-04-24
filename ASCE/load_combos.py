a11ab2ef5bc63864c77ce8df22d09f4859f89815
David Hanson <davidhanson.c@gmail.com>
2023-04-24 12:01:43 -0700

    first commit from new computer

--- ASCE/load_combos.py
+++ ASCE/load_combos.py
@@ -1,10 +1,11 @@
-from errno import EHOSTDOWN
-from re import L, S
-from this import d
-from tkinter import W
 import numpy as np
 
-def LRFD(D=0, L=0, Lr=0, S=0, R=0, W=0, Ev=0, Eh=0, dir="P"):
+def LRFD(D=0, L=0, Lr=0, S=0, R=0, H=0, W=0, Ev=0, Eh=0, dir="P", H_cond="max"):
+    H_factor = None
+    if H_cond == "max":
+        H_factor = 1.6
+    elif H_cond == "min":
+        H_factor = 0.9
     combos = [
         1.4*D,
         1.2*D + 1.6*L + 0.5*max(Lr, S, R),
@@ -16,12 +17,17 @@ def LRFD(D=0, L=0, Lr=0, S=0, R=0, W=0, Ev=0, Eh=0, dir="P"):
     ]
     ultimate_load = 0
     if dir=="P":
-        ultimate_load = max(combos)
+        ultimate_load = max(combos) + H*H_factor
     elif dir=="N":
-        ultimate_load = min(combos)
+        ultimate_load = min(combos) + H*H_factor
     return ultimate_load
 
-def ASD(D=0, L=0, Lr=0, S=0, R=0, W=0, Ev=0, Eh=0, dir="P"):
+def ASD(D=0, L=0, Lr=0, S=0, R=0, H=0, W=0, Ev=0, Eh=0, dir="P", H_cond="max"):
+    H_factor = None
+    if H_cond == "max":
+        H_factor = 1.0
+    elif H_cond == "min":
+        H_factor = 0.6
     combos = [
         D,
         D + L,
@@ -36,7 +42,7 @@ def ASD(D=0, L=0, Lr=0, S=0, R=0, W=0, Ev=0, Eh=0, dir="P"):
     ]
     allowable_load = 0
     if dir=="P":
-        allowable_load = max(combos)
+        allowable_load = max(combos) + H*H_factor
     elif dir=="N":
-        allowable_load = min(combos)
+        allowable_load = min(combos) + H*H_factor
     return allowable_load
--- template_code.py
+++ template_code.py
@@ -1,50 +1,10 @@
-# Latex in Jupyter Notebook
-from IPython.display import display, Math, Markdown, Latex
-import numpy as np
-import pandas as pd
-display(Latex(r"\newpage")) #pagebreak
-display(Latex(f'$x_{i}$')) #math display
-display(Markdown('<img src="image.png" style="height: 300px;"/>'))
-
-# Importing my own module
-import os, sys
-
-p = os.path.abspath('.')
-sys.path.insert(1, p)
-import mymodule
-
-import matplotlib as plt
-%matplotlib inline
-
-# import beam and supports
-from indeterminatebeam import Beam, Support
-
-# import loads (all load types imported for reference)
-from indeterminatebeam import (
-    PointTorque,
-    PointLoad,
-    PointLoadV,
-    PointLoadH,
-    UDL,
-    UDLV,
-    UDLH,
-    TrapezoidalLoad,
-    TrapezoidalLoadV,
-    TrapezoidalLoadH,
-    DistributedLoad,
-    DistributedLoadV,
-    DistributedLoadH
-)
-
-
-### EXAMPLE HEADER ###
 # Import Python Tools:
 import pandas as pd
 import numpy as np
 from numpy import sqrt, pi, average, cos, sin, tan, arcsin, arccos, arctan, deg2rad, rad2deg
 # import sectionproperties as shape
 
-import ASCE.load_combos
+from ASCE import load_combos
 
 from steel_design.AISC import aisc
 steel_density = 0.2833 #pci
