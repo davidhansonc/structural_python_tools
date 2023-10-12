5bf278874f1cc957f3bfcbd998e4b805be36cabf
David Hanson <davidhanson.c@gmail.com>
2023-10-12 09:34:33 -0700

    photo_convert

--- steel_design/AISC.py
+++ steel_design/AISC.py
@@ -9,7 +9,7 @@ shapes_database_file = ""
 if system == 'Darwin':
 	shapes_database_file = "/Users/davidhansonc/Desktop/structural_python_tools/steel_design/aisc-shapes-database-v15.0.xlsx"
 elif system == 'Windows':
-	shapes_database_file = r"C:\Users\dhanson\Desktop\Resources\structural_python_tools\steel_design\aisc-shapes-database-v15.0.xlsx"
+	shapes_database_file = r"C:\Users\david\structural_python_tools\steel_design\aisc-shapes-database-v15.0.xlsx"
 elif system == 'Linux':
 	shapes_database_file = "/Users/davidhansonc/Desktop/structural_python_tools/steel_design/aisc-shapes-database-v15.0.xlsx"
 else:
