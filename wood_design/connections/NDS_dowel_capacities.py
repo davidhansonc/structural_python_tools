2d6dcf6b2acc1f76085aa8e6f253533c6ecc2a51
davidhansonc <davidhanson.c@gmail.com>
2024-01-12 13:12:25 -0700

    mac updates

--- wood_design/connections/NDS_dowel_capacities.py
+++ wood_design/connections/NDS_dowel_capacities.py
@@ -7,7 +7,7 @@ import numpy as np
 from IPython.display import display, Latex
 
 
-def dowel_shear(Z_nominal, C_D=1.0, C_M=1.0, C_t=1.0, C_g=1.0, C_delta=1.0, C_eg=1.0, C_di=1.0, toe_nailing="N", print="N"):
+def dowel_shear(Z_nominal, C_D=1.0, C_M=1.0, C_t=1.0, C_g=1.0, C_delta=1.0, C_eg=1.0, C_di=1.0, toe_nailing="N"):
     '''
     - NDS Table 11.3.1
     - All adjustment factors are 1.0 by default
@@ -17,10 +17,6 @@ def dowel_shear(Z_nominal, C_D=1.0, C_M=1.0, C_t=1.0, C_g=1.0, C_delta=1.0, C_eg
     else:
         C_tn = 1.0 
     Z_factored = Z_nominal * C_D * C_M * C_t * C_g * C_delta * C_eg * C_di * C_tn
-    if print == "Y":
-        display(Latex("$Z'=Z C_D C_M C_t C_g C_{\\delta} C_{eg} C_{di}$"))
-        display(Latex(f"$Z'= {Z_nominal} lbs * {C_D} * {C_M} * {C_t} * {round(C_g, 3)} * {C_delta} * {C_eg} * {C_di} * {C_tn} $"))
-        display(Latex(f"$Z' = {round(Z_factored, 2)} lbs$"))
     return Z_factored
 
 
