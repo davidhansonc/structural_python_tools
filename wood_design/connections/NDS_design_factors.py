9e56df75c7f76b900e4028ab44a6a967d69454fb
davidhansonc <davidhanson.c@gmail.com>
2024-02-06 12:56:12 -0700

    update

--- wood_design/connections/NDS_design_factors.py
+++ wood_design/connections/NDS_design_factors.py
@@ -1,21 +1,25 @@
 import numpy as np
 
+def group_factor(conn_type, D, n, s, Am, As, Em=1.6*10**6, Es="default"):
+    '''
+    Calculates the Group Action Factor (C_g) for dowel type fasteners with a diameter <= 1/4".
 
-# For fasteners with D <= 1"
-def group_factor(conn_type, D, n, s, Am, As, Em=1.6*10**6, Es="default`"):
+    Parameters:
+    - conn_type (str): "ww" (wood-to-wood) or "wm" (wood-to-metal)
+    - D (float): Dowel diameter (in)
+    - n (int): Number of dowels in a line
+    - s (float): Dowel spacing on center (in)
+    - Am (float): Main member cross-sectional area (in^2)
+    - As (float): Side member cross-sectional area (in^2)
+    - Em (float): Main member elastic modulus (psi), default for DF #2
+    - Es (float or str): Side member elastic modulus (psi), default for DF #2 or for steel
+
+    Returns:
+    - float: The Group Action Factor (C_g).
     '''
-    - Calculation is valid for dowel type fasteners with a diameter <= 1/4"
-    - conn_type = "ww" (wood-to-wood) or "wm" (wood-to-metal)
-    - D = dowel diameter (in)
-    - n = number of dowels in a line
-    - s = dowel spacing on center (in)
-    - Em = main member elastic modulus (psi) (default for DF #2)
-    - Es = side member elastic modulus (psi) (default for DF #2 or for steel)
-    - Am = main member cross-sectional area (in^2) (default for 2x6)
-    - As = side member cross-sectional area (in^2) (default for 2x6)
-    '''
-    if D < 0.25:
+    if D <= 0.25:
         C_g = 1.0
+        print("C_g = 1.0 for D <= 0.25 inches")
         return C_g
 
     gamma = 0
@@ -26,13 +30,20 @@ def group_factor(conn_type, D, n, s, Am, As, Em=1.6*10**6, Es="default`"):
                 Es = 1.6*10**6
         elif conn_type == "wm":
             gamma = 270000 * D**1.5
-            Es = 29000000
+            if Es == "default":
+                Es = 29000000
     except:
         print('Invalid connection type. Try "ww" (wood-to-wood) or "wm" (wood-to-metal)"')
+        return None
+
     u = 1 + gamma * s / 2 * (1 / (Em * Am) + 1 / (Es * As))
     m = u - np.sqrt(u**2 - 1)
     R_ea = min(Es * As / Em / Am, Em * Am / Es / As)
 
-    C_g = (m / n * (1 - m**(2*n)) / ((1 + R_ea * m**n) * (1 + m) - 1 + m**(2*n))) \
-        * ((1 + R_ea) / (1 - m))
+    C_g = (m / n * (1 - m**(2*n)) / ((1 + R_ea * m**n) * (1 + m) - 1 + m**(2*n))) * ((1 + R_ea) / (1 - m))
+    
+    # Print the formula used for calculation
+    print(f"C_g Formula used: C_g = (m / n * (1 - m^(2*n)) / ((1 + R_ea * m^n) * (1 + m) - 1 + m^(2*n))) * ((1 + R_ea) / (1 - m))")
+    print(f"Where m = {round(m, 3)}, R_ea = {round(R_ea, 3)}, n = {n}, and gamma = {round(gamma, 1)}")
+    
     return C_g
